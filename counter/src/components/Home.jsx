635980e639760f987f58bf7e8ba12a530c2f13e8
mudasir <mudasirahmad32@gmail.com>
2025-07-02 19:55:56 +0530

    Mu

--- counter/src/components/Home.jsx
+++ counter/src/components/Home.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Home() {
+
+
   return (
     <div>
+      <button>CLick me </button>
         <h1>Welcome to home </h1>
+        <Mudi/>
     </div>
+  )
+}
+function Mudi() {
+  return(
+    <h2>Google</h2>
   )
 }
 
